f0702679038d40b7dc2a166240debcbba0543aa9
Valerii Vitenko <valero2313@gmail.com>
2022-05-12 12:10:45 +0300

    brought the basic server to life

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const { PORT } = process.env;
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
